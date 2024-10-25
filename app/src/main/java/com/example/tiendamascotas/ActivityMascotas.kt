6ca637e4b28252ce7fae83a85ef4e1fafdbe5ea4
DanielOrtiz04 <dortic13@miumg.edu.gt>
2024-10-24 22:24:37 -0600

    Creamos el ActivityMascotas.kt

--- app/src/main/java/com/example/tiendamascotas/ActivityMascotas.kt
+++ app/src/main/java/com/example/tiendamascotas/ActivityMascotas.kt
@@ -11,9 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tiendamascotas.ui.theme.TiendaMascotasTheme
-import com.example.tiendamascotas.view.ViewComidas
+import com.example.tiendamascotas.view.ViewMascotas
 
-class ActivityComidas : ComponentActivity() {
+class ActivityMascotas : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,13 +23,19 @@ class ActivityComidas : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ViewComidas()
+                    ViewMascotas()
                 }
             }
         }
     }
 }
 
-class ViewComidas {
 
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview5() {
+    TiendaMascotasTheme {
+        ViewMascotas()
+    }
 }
