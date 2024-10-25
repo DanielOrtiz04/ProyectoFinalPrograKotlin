0cb3badb40b865dd8989253c50f85454737ed6d6
DanielOrtiz04 <dortic13@miumg.edu.gt>
2024-10-24 21:49:31 -0600

    Subimos el Proyecto

---
+++ Pet_Shop-master/app/src/main/java/com/example/tiendamascotas/view/ViewMascotas.kt
@@ -0,0 +1,133 @@
+package com.example.tiendamascotas.view
+
+import android.content.Intent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tiendamascotas.ListActivity
+import com.example.tiendamascotas.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ViewMascotas() {
+
+    val mContexto = LocalContext.current
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                ),
+                title = {
+                    Text("Mascotitas !SHOP PET¡")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = "Tipos de razas",
+                )
+            }
+        },
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(20.dp),
+        ) {
+            Text(text = "Gato de raza burmese", fontSize = 20.sp)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp) // Ajusta la altura según tus necesidades
+                    .background(MaterialTheme.colorScheme.primary) // Color de fondo del Box
+                    .padding(16.dp), // Espaciado interno
+                contentAlignment = Alignment.Center, // Centra el contenido
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.burmese3cat),
+                    contentDescription = null,
+                    modifier = Modifier
+
+                        .clickable {
+                            mContexto.startActivity(
+                                Intent(mContexto, ListActivity::class.java)
+                            )
+                        }
+
+                        .clip(RoundedCornerShape(16.dp)) // Esquinas redondeadas
+                )
+            }
+            Text(text = "Perro de raza Chiguagua", fontSize = 20.sp)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp) // Ajusta la altura según tus necesidades
+                    .background(MaterialTheme.colorScheme.primary) // Color de fondo del Box
+                    .padding(16.dp), // Espaciado interno
+                contentAlignment = Alignment.Center, // Centra el contenido
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.dog3),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(16.dp)) // Esquinas redondeadas
+
+                        .clickable { mContexto.startActivity(
+                            Intent(mContexto, ListActivity::class.java)
+                        ) }
+                )
+            }
+            Text(text = "Tortuga acuatica", fontSize = 20.sp)
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp) // Ajusta la altura
+                    .background(MaterialTheme.colorScheme.primary) // Color de fondo del Box
+                    .padding(16.dp), // Espaciado interno
+                contentAlignment = Alignment.Center, // Centra el contenido
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.turtle3),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clip(RoundedCornerShape(16.dp)) // Esquinas redondeadas
+
+                        .clickable { mContexto.startActivity(
+                            Intent(mContexto, ListActivity::class.java)
+                        ) }
+                )
+            }
+        }
+    }
+}
